7be91e2b10f8d629ff13c88caf0ed6b9adf451a5
jclayton128 <jclayton128@gmail.com>
2021-05-08 22:46:58 -0400

    Attacks spawn projectiles on both clients

--- Assets/Attack_Basic.cs
+++ Assets/Attack_Basic.cs
@@ -25,8 +25,10 @@ public class Attack_Basic : Attack
     public override void CmdRequestAttackCommence()
     {
         Debug.Log("request attack");
-        //Client-side Shot validation here, if any.
-        if (!hasAuthority) { return; }
+        if (!hasAuthority) { return; }  // This check isn't necessary because a computer must have authority to call a command.
+
+        // Client-side Shot validation here, if any. 
+
         ExecuteAttackOnServer();
         //ExecuteAttackOnClient();  //This should be a target RPC to set the simulated attack on the client's screen, but without any colliders on bullets.
     }
@@ -44,17 +46,30 @@ public class Attack_Basic : Attack
     [Server]
     protected override void ExecuteAttackOnServer()
     {
-        //Server-side validation of shot. Time between shots? Enough energy?
+        // Server-side validation of shot. Sufficient time between shots? Enough energy? Perhaps take in client's believed position and allow it
+        // to be the source of the shot as long as its within a certain range of server-true position.
         if (timeSinceLastAttack < 0)
         {
             Debug.Log("execute attack on server");
             GameObject bullet = Instantiate(projectilePrefab, transform.position + (transform.up * offset), transform.rotation) as GameObject;
             bullet.GetComponent<Rigidbody2D>().velocity = bullet.transform.up * weaponSpeed;
-            NetworkServer.Spawn(bullet);
+            //NetworkServer.Spawn(bullet);
             //NetworkServer.Destroy(shell); //TODO find a way to destroy these objects via NetworkServer.Destroy after the lifetime is met
-            //Destroy(shell, weaponLifetime);
+            Destroy(bullet, weaponLifetime);
             timeSinceLastAttack = timeBetweenAttacks;
+
+            RpcDisplaySimAttackOnClients();
         }
+    }
+
+    [ClientRpc]
+    private void RpcDisplaySimAttackOnClients()
+    {
+        GameObject bullet = Instantiate(projectilePrefab, transform.position + (transform.up * offset), transform.rotation) as GameObject;
+        bullet.GetComponent<Rigidbody2D>().velocity = bullet.transform.up * weaponSpeed;
+        //turn off spawned bullet's collider since the client's instance is simulated and not true
+        Destroy(bullet, weaponLifetime);
+
     }
 
 }
